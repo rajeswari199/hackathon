169552478830a721d84ea96c6ee7b6f62ec66a0e
rajeswari <raji.thavamuthu1196@gmail.com>
2022-03-25 09:44:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
+
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+
+// AppRegistry.registerComponent('com.financial.forecast', () => App);
